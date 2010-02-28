66ae11f55fc7d7c0a3847408c633f159ad7b72fe
Topher <topher@topher.local>
2010-02-27 20:07:25 -0700

    fixed route

--- app.rb
+++ app.rb
@@ -3,27 +3,28 @@ require 'sinatra'
 require 'builder'
 
 get '/' do
-  'HighLow is a <a href="teleku.com">Teleku Voice Application</a>'
+  'HighLow is a <a href="teleku.com">Teleku Voice Application</a><br>'
+  'curl --data-urlencode "caller=test" http://highlow.heroku.com'
 end
 
 post '/' do 
-  session[:deal] = 1 + rand(10)
+  deal = 1 + rand(10)
   builder do |xml|
     xml.instruct!
     xml.phoneml do
       xml.speak "welcome to the game of high low"
-      xml.speak "the dealer randomly selected " + session[:deal].to_s
+      xml.speak "the dealer randomly selected " + deal.to_s
       xml.speak "will his next number be higher or lower? press 1 or say higher or press 2 or say lower"
-      xml.input "http://highlow.heroku.com/guess", "options"=>"1,2,higher,lower"
+      xml.input "http://highlow.heroku.com/guess/" + deal.to_s, "options"=>"1,2,higher,lower"
     end
   end
 end
 
-post '/guess' do 
-  session[:newdeal] = 1 + rand(10)
+post '/guess/:deal' do 
+  newdeal = 1 + rand(10)
   guess = params[:callerinput]
   if guess == '1' or guess == 'higher'
-    if session[:newdeal] > session[:deal]
+    if newdeal > params[:deal].to_i 
       gamestatus = "winner"
     else
       gamestatus = "loser"
@@ -31,22 +32,20 @@ post '/guess' do
   end  
 
   if guess == '2' or guess == 'lower'
-    if session[:newdeal] < session[:deal]
+    if newdeal < params[:deal].to_i  
       gamestatus = "winner"
     else
       gamestatus = "loser"
     end
   end  
     
-  session[:deal] = session[:newdeal]
-  
   builder do |xml|
     xml.instruct!
     xml.phoneml do
-      xml.speak "the dealer randomly selected " + session[:deal].to_s
+      xml.speak "the dealer randomly selected " + params[:deal].to_s
       xml.speak "you are a " + gamestatus     
       xml.speak "will his next number be higher or lower? press 1 or say higher or press 2 or say lower"
-      xml.input "http://highlow.heroku.com/guess", "options"=>"1,2,higher,lower"
+      xml.input "http://highlow.heroku.com/guess/" + newdeal.to_s, "options"=>"1,2,higher,lower"
     end
   end
 end
